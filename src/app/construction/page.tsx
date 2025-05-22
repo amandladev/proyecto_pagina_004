ee33023c62e94bfee6e33fc65f771444ea39b8d9
Серхио <xt8634@rimac.com.pe>
2025-05-21 21:31:50 -0500

    cambios de seo v2

--- src/app/construction/page.tsx
+++ src/app/construction/page.tsx
@@ -1,17 +1,29 @@
 import { Construction } from "lucide-react";
-
+import Head from "next/head";
 export default function ConstructionPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center p-6">
-      <div className="bg-white shadow-xl rounded-2xl p-10 max-w-md w-full">
-        <Construction className="h-16 w-16 text-yellow-500 mx-auto mb-6" />
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">¡En construcción!</h1>
-        <p className="text-gray-600 text-lg">
-          Estamos trabajando en esta sección para brindarte la mejor experiencia posible. 
-          Vuelve pronto para ver las novedades.
-        </p>
-        <p className="mt-6 text-sm text-gray-400">Gracias por tu paciencia 💛</p>
+    <>
+      <Head>
+        <title>Azochem</title>
+        <meta name="description" content="Soluciones de limpieza industrial." />
+        <meta property="og:title" content="Marketplace en construcción | Azochem" />
+        <meta property="og:description" content="Venta de soluciones de limpieza para el hogar." />
+        <meta property="og:image" content="https://www.azochem.com/images/logo_azochem.png" />
+        <meta property="og:url" content="https://www.azochem.com/" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center p-6">
+        <div className="bg-white shadow-xl rounded-2xl p-10 max-w-md w-full">
+          <Construction className="h-16 w-16 text-yellow-500 mx-auto mb-6" />
+          <h1 className="text-3xl font-bold text-gray-800 mb-4">¡En construcción!</h1>
+          <p className="text-gray-600 text-lg">
+            Estamos trabajando en esta sección para brindarte la mejor experiencia posible.
+            Vuelve pronto para ver las novedades.
+          </p>
+          <p className="mt-6 text-sm text-gray-400">Gracias por tu paciencia 💛</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import ContactSection from "@/components/ui/contact/Contact";
 import Navbar from "@/components/ui/navbar/Navbar";
 import ServicesSection from "@/components/ui/services/Services";
 import Head from "next/head";
-
 export default function Home() {
   
   return (
@@ -15,8 +14,11 @@ export default function Home() {
       <Head>
         <title>Azochem</title>
         <meta name="description" content="Soluciones de limpieza industrial." />
-        <meta property="og:title" content="Azochem" />
-        <meta property="og:description" content="Soluciones de limpieza industrial." />
+        <meta property="og:title" content="Productos de Limpieza Industrial | Azochem" />
+        <meta property="og:description" content="Soluciones ecológicas y efectivas para la limpieza industrial." />
+        <meta property="og:image" content="https://www.azochem.com/images/logo_azochem.png" />
+        <meta property="og:url" content="https://www.azochem.com/" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <main className=" ">
           <Navbar />
