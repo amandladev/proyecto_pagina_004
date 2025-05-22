da4f072e475db9896404fab48e0a5ddcd807f301
Серхио <xt8634@rimac.com.pe>
2025-05-21 21:18:36 -0500

    Cambios head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import BrandShowcaseSection from "@/components/brandShowcase/Brands";
 import ConsumerProductsSection from "@/components/consumerProducts/consumer-products";
 import ElegantFooter from "@/components/footer/Footer";
 import AboutSection from "@/components/ui/about/About";
@@ -7,42 +6,28 @@ import ClientsSection from "@/components/ui/clients/Clients";
 import ContactSection from "@/components/ui/contact/Contact";
 import Navbar from "@/components/ui/navbar/Navbar";
 import ServicesSection from "@/components/ui/services/Services";
+import Head from "next/head";
 
 export default function Home() {
-  // const carouselItems = [
-  //   {
-  //     image: "/test_img_.jpg",
-  //     subtitle: "Detergentes para",
-  //     title: "Titulo de tipo de producto",
-  //   },
-  //   {
-  //     image: "/test_img2.jpg", // Replace with your actual images
-  //     subtitle: "Limpiadores para",
-  //     title: "Superficies brillantes",
-  //   },
-  //   {
-  //     image: "/test_img3.jpg",
-  //     subtitle: "Soluciones para",
-  //     title: "Limpieza industrial",
-  //   },
-  //   {
-  //     image: "/test_img4.jpg",
-  //     subtitle: "Productos para",
-  //     title: "Cuidado del hogar",
-  //   },
-  // ]
+  
   return (
-    <main className=" ">
-        <Navbar />
-        <ClientsSection />
-        {/* <BrandShowcaseSection /> */}
-        {/* <ImageCarousel items={carouselItems} /> */}
-        <AboutSection />
-        <ServicesSection />
-        <ExpertAdviceSection />
-        <ConsumerProductsSection />
-        <ContactSection />
-        <ElegantFooter />
+    <>
+      <Head>
+        <title>Azochem</title>
+        <meta name="description" content="Soluciones de limpieza industrial." />
+        <meta property="og:title" content="Azochem" />
+        <meta property="og:description" content="Soluciones de limpieza industrial." />
+      </Head>
+      <main className=" ">
+          <Navbar />
+          <ClientsSection />
+          <AboutSection />
+          <ServicesSection />
+          <ExpertAdviceSection />
+          <ConsumerProductsSection />
+          <ContactSection />
+          <ElegantFooter />
       </main>
+    </>
   );
 }
